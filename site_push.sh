21da1bb71a729e3b4270fa6781880681e37500a5
Randy Bias <randyb@cloudscaling.com>
2014-08-07 19:37:10 -0700

    tweaks

--- site_push.sh
+++ site_push.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 #
-unset GIT_DIR
-unset GIT_WORK_TREE
-export S3_BUCKET="disruptivebias"
+export S3_BUCKET="disruptivebias.com"
 export TEMP_DEPLOY_DIR="/tmp/${S3_BUCKET}"
-export GIT_DIR=$(pwd)
-export GIT_WORK_TREE=${TEMP_DEPLOY_DIR}
-export S3CMD_CFG="${GIT_DIR}/.s3cfg"
+export S3CMD_CFG="$(pwd)/.s3cfg"
 export JEKYLL_CMD="/usr/local/Cellar/ruby/2.1.1/bin/jekyll"
 
 rm -rf ${TEMP_DEPLOY_DIR}
 
 mkdir -p ${TEMP_DEPLOY_DIR}
-git checkout -f
-pushd ${TEMP_DEPLOY_DIR}
 
+git clone $(pwd) ${TEMP_DEPLOY_DIR}
+pushd ${TEMP_DEPLOY_DIR}
+git submodule update --init --recursive
 ${JEKYLL_CMD} build
-/usr/bin/env s3cmd -c ${S3CMD_CFG} sync --delete-removed --acl-public --exclude '.git*' --exclude '.s3cfg' _site/ s3://${S3_BUCKET}
+/usr/bin/env s3cmd -c ${S3CMD_CFG} sync --delete-removed --acl-public --exclude '.git*' --exclude '.s3cfg' --exclude 'site_push.sh' _site/ s3://${S3_BUCKET}
 
 popd
